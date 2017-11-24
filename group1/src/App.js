b9d0e180d09f08061773bce3c59771567a9f2952
Jules Aubry <Jules Aubry>
2017-11-24 12:55:40 +0200

    First commit (test)

--- group1/src/App.js
+++ group1/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h2 className="App-title">Welcome to our group project</h2>
+          <h3 className="App-subTitle">Our team members are Jules Aubry, Otto Hyvönen and Kamal Lamsal. </h3>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
