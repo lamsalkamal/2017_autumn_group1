8dddff4a3ded8fa7486c20ad61df1375e35e24b4
Jules Aubry <Jules Aubry>
2017-11-28 13:54:30 +0200

    RegionLevel is now working, have to check with what Otto did

--- group1/src/App.js
+++ group1/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.js';
-
+import RegionLevel from './components/RegionLevel.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header > <Header/> </header>
+        
+        <RegionLevel/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
