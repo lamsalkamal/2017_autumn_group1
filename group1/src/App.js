dd7f4023c24229b42f687a4bb42932a4a63e93ee
Jules Aubry <Jules Aubry>
2017-12-01 10:53:48 +0200

    New name : leftbar.js is our working project

--- group1/src/App.js
+++ group1/src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 
 //import Body from './components/body.js'
 //import {Row,Grid,Col ,Button } from 'react-bootstrap'
-import Scenario from './components/TestLeftBar.js'
+import Scenario from './components/LeftBar.js'
 
 class App extends Component {
   render(
--- group1/src/components/leftbar.js
+++ group1/src/components/leftbar.js
@@ -1,77 +1,161 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import './leftbar.css';
-import {Row,Grid,Col,Button,FormControl,FormGroup} from 'react-bootstrap'
-import leftbarItem from './leftbarItem1'
-import apiData from './Data/getData'
+class RegionLevel extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+          regionsLevels : []
+        };
 
-export default class Leftbar extends Component {
-
-constructor(props)
-{
-  super(props);
-
-  this.state = {
-    items: []
-  };
- 
-  this.fetchData = this.fetchData.bind(this);
-}
-
-fetchData() //leftbar.js:23 Uncaught TypeError: __WEBPACK_IMPORTED_MODULE_4__Data_getData__.a.getRegionlevels is not a function
-{           //works in test app but not in this?
-    apiData.getRegionlevels().then(result => {
-    this.setState( { items : result} );
-    });
-    console.log(this.state.items);    
-}
-
-componentDidMount() 
-{   
-    //this.fetchData();
-    console.log(this.state.items); 
-}
-
-render() {
-      
-    return (
-
-<Col xm="12" sm="3" className="leftbar">
-<button className="btn btn-default"
-                                onClick={ this.fecthData } >testButton</button>
-<h3 class="leftHeadPadding"> Selecting Scenario </h3>
-<h4>Regional Level</h4>
-<FormGroup controlId="formControlSelect">
- <FormControl componentClass="select" placeholder="select">
-      { this.state.items.map(element => <leftbarItem description={element.description}
-                                                name={element.name} 
-                                                order={element.order}
-                                                id={element.id}
-                                                key={element.id} /> )}
- </FormControl>
-</FormGroup>
-<h4>Area </h4>
-<FormGroup controlId="formControlSelect">
- <FormControl componentClass="select" placeholder="select">
-     <option value=""> Select </option>
-     <option value="option1"> Northen Ostrobonia </option>
-     <option value="option2"> option2 </option>
-     <option value="option3"> option3 </option>
-     <option value="option4"> option4 </option>
- </FormControl>
-</FormGroup>
-<h4>Scenario Collection </h4>
-<FormGroup controlId="formControlSelect">
- <FormControl componentClass="select" placeholder="select">
-     <option value=""> Select </option>
-     <option value="option1"> option1 </option>
-     <option value="option2"> option2 </option>
-     <option value="option3"> option3 </option>
-     <option value="option4"> option4 </option>
- </FormControl>
-</FormGroup>
-</Col>
-
-      )
+        this.change = this.change.bind(this);
     }
-}
+  
+    componentDidMount() {
+      this.RegionLevel();
+    }
+  
+    RegionLevel() {
+      fetch(`http://melatupa.azurewebsites.net/regionLevels`)
+ 		.then(result=>result.json())
+        .then(regionsLevels=>this.setState({regionsLevels}))
+    }
+
+    change(event){
+        var value = event.target.value;
+        this.props.updateRegionLevel(value);
+     }
+  
+    render() {
+      const regionLevels = this.state.regionsLevels.map(item=><option key={item.id} value={item.id}>{item.name}</option>)
+      return (
+        <div>
+        Aluetaso 
+            <select id="regionLevelId" onChange={this.change}>
+            <option disabled selected="selected"> -- select an option -- </option>
+                {regionLevels}
+            </select>         
+        </div>
+      );
+    }
+  }
+
+class Region extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+          regionLevel : "1",
+          regions : [],
+          scenariosCollection : [],
+          regionId : "1"
+        };
+
+        this.handleRegionUpdate = this.handleRegionUpdate.bind(this);
+        this.change = this.change.bind(this);
+        this.changeScenarioCollectionId = this.changeScenarioCollectionId.bind(this);
+    }
+  
+    componentDidMount() {
+      this.RegionLevel();
+    }
+  
+    RegionLevel() {
+      /*
+        fetch(`http://melatupa.azurewebsites.net/regionLevels/${this.state.regionLevel}/regions`)
+        .then(result=>result.json())
+        .then(regions=>this.setState({regions}))*/
+    }
+
+    handleRegionUpdate(regLevel) {
+      this.setState({regionLevel : regLevel}, function() {
+        fetch(`http://melatupa.azurewebsites.net/regionLevels/${this.state.regionLevel}/regions`)
+        .then(result=>result.json())
+        .then(regions=>this.setState({regions}))
+      });
+    }
+
+    change(event){
+      var index = event.nativeEvent.target.selectedIndex;
+      var scenariosArray = this.state.regions[index].scenarioCollections;
+      this.setState({scenariosCollection : scenariosArray}, function() {
+      });
+      this.setState({regionId : event.target.value}, function() {
+      });
+   }   
+
+    changeScenarioCollectionId(event) {
+      this.props.updateScenarioCollection(event.target.value, this.state.regionId);
+   }
+
+   
+    render() {  
+
+      const regions = this.state.regions.map(item=><option key={item.id} value={item.id}>{item.name}</option>)
+      const scenariosCollection = this.state.scenariosCollection.map(item=><option key={item.id} value={item.id}>{item.name}</option>)
+      return (
+        <div>
+            <RegionLevel updateRegionLevel={this.handleRegionUpdate}/> 
+            Alue
+           <select id="regionId" onChange={this.change}>
+             {regions}
+           </select>   
+           <hr/>
+           Skenaariokokoelma
+           <select id="scenarioCollectionId" onChange={this.changeScenarioCollectionId}>  
+              {scenariosCollection}
+           </select>          
+        </div>
+      );
+    }
+  }
+
+  class Scenario extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        scenariosA : []
+        };
+
+        this.updateScenarioCollectionId = this.updateScenarioCollectionId.bind(this);
+    }
+  
+    componentDidMount() {
+      this.Scenario();
+    }
+  
+    Scenario() {
+    }
+
+    updateScenarioCollectionId(scenarioCollectionId, regionId) {
+        fetch(`http://melatupa.azurewebsites.net/scenarioCollection/${scenarioCollectionId}/region/${regionId}`)
+        .then(result=>result.json())
+        .then(scenariosA=>this.setState({scenariosA}))
+    }
+  
+    render() {
+     const contentScenarios = this.state.scenariosA.length>0 ? this.state.scenariosA[0].scenarios : []
+     const scenarios = contentScenarios.map(item=><li key={item.id} value={item.name}>{item.description}</li>)
+
+     const contentDates = this.state.scenariosA.length>0 ? this.state.scenariosA[0].timePeriods : []
+     const periods = contentDates.map(item=><li key={item.id}>{item.yearStart} - {item.yearEnd}</li>)
+
+      return (
+        <div id="layout-content" className="layout-content-wrapper">
+            <Region updateScenarioCollection={this.updateScenarioCollectionId}/> 
+            
+            <ul id="scenarioId" >
+              {scenarios}
+           </ul>   
+
+           <ul id="periodId" >
+              {periods}
+           </ul>  
+                  
+        </div>
+      );
+    }
+  }
+
+  export default Scenario
